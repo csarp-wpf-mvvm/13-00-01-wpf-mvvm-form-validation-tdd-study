67eddf374b07735a77e8525172fa00fb68bc7694
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-12 12:43:30 +0200

    IsOnlyLetter elemi megoldás

--- Kreata.Desktop/Validation/ValidationRules/NameValidationRules.cs
+++ Kreata.Desktop/Validation/ValidationRules/NameValidationRules.cs
@@ -16,7 +16,19 @@ namespace Kreta.Desktop.Validation.ValidationRules
 
         public bool IsNameShort => _nameToValidate.Length < 2;
         public bool IsOnlyLetterOrSpaceOrDash => false;
-        public bool IsOnlyLetter => false;
+        public bool IsOnlyLetter
+        {
+            get
+            {
+                if (!_nameToValidate.Any())
+                    return false;
+                foreach (char c in _nameToValidate)
+                    if (!char.IsLetter(c))
+                        return false;
+                return true;
+            }
+        }
+
         public bool IsThereSpaceInTheName => false;
         public bool IsThereDashInTheName => false;
 
