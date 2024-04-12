2f1fbcc0fc5b8d68008fce99232fcf3b73ebd7eb
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-12 13:11:32 +0200

    Egyenlőre a feladatot elemi megoldással oldjuk meg

--- Kreata.Desktop/Validation/ValidationRules/NameValidationRules.cs
+++ Kreata.Desktop/Validation/ValidationRules/NameValidationRules.cs
@@ -17,7 +17,28 @@ namespace Kreta.Desktop.Validation.ValidationRules
         public bool IsNameShort => _nameToValidate.Length < 2;
         //Acs-Sánt*
         //Farkasné Sz8bó
-        public bool IsOnlyLetterOrSpaceOrDash => IsOnlyLetter || IsThereDashInTheName || IsThereSpaceInTheName;
+        public bool IsOnlyLetterOrSpaceOrDash
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_nameToValidate))
+                    return false;
+                for (int i = 0; i < _nameToValidate.Length; i++)
+                {
+                    if (i == 0 || i == _nameToValidate.Length - 1)
+                    {
+                        if (!char.IsLetter(_nameToValidate[i]))
+                            return false;
+                    }
+                    else
+                    {
+                        if (!(char.IsLetter(_nameToValidate[i]) || _nameToValidate[i] == ' ' || _nameToValidate[i] == '-'))
+                            return false;
+                    }
+                }
+                return true;
+            }
+        }
         public bool IsOnlyLetter => !string.IsNullOrEmpty(_nameToValidate) ? _nameToValidate.All(char.IsLetter) : false;
 
         public bool IsThereSpaceInTheName => 
